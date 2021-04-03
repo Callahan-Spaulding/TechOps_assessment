21f9ec66de23fb2ea13832fbc4de7c76ce367b06
Callahan-Spaulding <43965189+Callahan-Spaulding@users.noreply.github.com>
2021-04-03 10:12:15 -0500

    Add files via upload

--- Question 1/question_1.py
+++ Question 1/question_1.py
@@ -1,1 +1,11 @@
+import re
+import sys
 
+with open("test.txt", "r") as in_file:
+    sys.stdout = open('out.txt', 'w')
+    for line in in_file:
+        striped_line = line.strip()
+        res = re.findall(r'\w+', striped_line)
+        print(res[4])
+    in_file.close()
+    sys.stdout.close()
