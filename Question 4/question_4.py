758d578156fb4ba520cb25abc5f33684b2814900
Callahan-Spaulding <43965189+Callahan-Spaulding@users.noreply.github.com>
2021-04-03 10:13:47 -0500

    Add files via upload

--- Question 4/question_4.py
+++ Question 4/question_4.py
@@ -1,1 +1,29 @@
+import re
+import sys
 
+with open("test4_Fixlog.log", "r") as in_file:
+    sys.stdout = open('out4.txt', 'w')
+
+    storage = []
+
+
+    for line in in_file:
+        striped_line = line.strip()
+        line = striped_line.split("|")
+        for elem in line:
+            tag = elem.split("=")
+            if tag[0] == "52":
+                curr_code = tag[1]
+            
+                if curr_code in storage:
+                    break
+                else:
+                    print curr_code
+                    storage.append(curr_code)
+                    break
+            else:
+                continue
+
+
+    in_file.close()
+    sys.stdout.close()
