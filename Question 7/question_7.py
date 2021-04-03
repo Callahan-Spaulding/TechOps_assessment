7c923ae7cfb14dc6be76a613b311cc659ffa763b
Callahan-Spaulding <43965189+Callahan-Spaulding@users.noreply.github.com>
2021-04-03 10:15:21 -0500

    Add files via upload

--- Question 7/question_7.py
+++ Question 7/question_7.py
@@ -1,1 +1,22 @@
+import re
+import sys
 
+with open("gelber_ops.log", "r") as in_file:
+    sys.stdout = open('out7.txt', 'w')
+
+    
+
+    for line in in_file:
+        striped_line = line.strip()
+        if '[warn]' in striped_line.lower():
+            print striped_line
+        elif '[error]' in striped_line.lower():
+            print striped_line
+        elif '[fatal]' in striped_line.lower():
+            print striped_line
+        
+
+
+
+    sys.stdout.close()
+    in_file.close()
