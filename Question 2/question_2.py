3abd504f8e8549129840f0ce79cb5a6f085ee7f2
Callahan-Spaulding <43965189+Callahan-Spaulding@users.noreply.github.com>
2021-04-03 10:12:55 -0500

    Add files via upload

--- Question 2/question_2.py
+++ Question 2/question_2.py
@@ -1,1 +1,21 @@
+import re
+import sys
 
+with open("test2.txt", "r") as in_file:
+    sys.stdout = open('out2.txt', 'w')
+
+    names = []
+
+    for line in in_file:
+        striped_line = line.strip()
+        res = re.findall(r'\w+', striped_line)
+        curr_name = res[4]
+        if curr_name in names:
+            continue
+        else:
+            names.append(curr_name)
+    names.sort()
+    for name  in names:
+        print(name)
+    in_file.close()
+    sys.stdout.close()
