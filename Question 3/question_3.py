42b851a7eabe3b337b8a4cf844ba55656d0bbdec
Callahan-Spaulding <43965189+Callahan-Spaulding@users.noreply.github.com>
2021-04-03 10:13:18 -0500

    Add files via upload

--- Question 3/question_3.py
+++ Question 3/question_3.py
@@ -1,1 +1,26 @@
+import re
+import sys
 
+with open('test3.txt', 'r') as in_file:
+    sys.stdout = open('out3.txt', 'w')
+
+    names = []
+    counter = 0
+    previous = ""
+
+    for line in in_file:
+        striped_line = line.strip()
+        if (counter % 2) == 0:
+            previous = striped_line
+            counter += 1
+            continue
+        else:
+            previous = previous + " " + striped_line
+            print(previous)
+            previous = ""
+            counter += 1
+            continue
+
+
+    in_file.close()
+    sys.stdout.close()
